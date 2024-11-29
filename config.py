ff204b66e637c108e1566d7335ffce06da8a9f34
leoleexh <leoxp2008@gmail.com>
2024-11-29 15:54:16 +0800

    修改配置文件

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(override=True)
 
 class Config:
     # ComfyUI服务地址，优先使用环境变量，否则使用默认值
-    COMFYUI_BASE_URL = os.environ.get('COMFYUI_BASE_URL', 'http://192.168.1.196:8188')
+    COMFYUI_BASE_URL = os.environ.get('COMFYUI_BASE_URL', 'http://localhost:8188')
     
     # Flask配置
     DEBUG = os.environ.get('FLASK_DEBUG', 'True').lower() == 'true'
